a912fee1630bd6268fce7d9d562ec0b5c3279714
basicprune <Firedemon198@gmail.com>
2023-03-08 15:34:23 +1100

    push
    push

--- buttons.js
+++ buttons.js
@@ -4,3 +4,24 @@ function myFunction() {
 function contactPage() {
     window.location = "/ContactPage.html";
 }
+function passwordCheck() {
+    
+    if (document.getElementById("pass").value == "Admin")
+    {
+        
+       var homePageButton= document.getElementById("HomePage");
+        var contactPageButton= document.getElementById("ContactPage");
+        homePageButton.style.display = "block"; 
+        contactPageButton.style.display = "block"; 
+    }
+    else
+    {
+        alert("Password Incorrect");
+    }
+}
+
+function close_window() {
+    if (confirm("Close Window?")) {
+      close();
+    }
+  }
